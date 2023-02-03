ae7d8c8fab17b6b67da09954ad730b0d094550ad
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-02-03 07:06:31 +0100

    ci: api-extractor basic setup

---
+++ src/subtract.ts
@@ -0,0 +1,10 @@
+/**
+ * Calculate the difference between two numbers
+ * @param a - first number
+ * @param b - second number
+ * @returns  number
+ * @beta
+ */
+export function subtract(a: number, b: number) {
+  return a - b;
+}
