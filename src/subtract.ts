78146d04d4e0e59f73e74d2e316c417b177caeae
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-02-24 23:00:36 +0100

    made changes

--- src/subtract.ts
+++ src/subtract.ts
@@ -2,7 +2,7 @@
  * Calculate the difference between two numbers
  * @param a - first number
  * @param b - second number
- * @returns  number
+ * @returns
  * @beta
  */
 export function subtract(a: number, b = 0) {
